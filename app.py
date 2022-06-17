5daac1575908f5d1b575465f3bdda0c3247c10d3
Tina khan <tinak6501@gmail.com>
2022-06-17 16:14:33 -0700

    First Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Step 3
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle 
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb')) 
+@app.route('/')
+def home():    
+   return render_template('page.html') 
+@app.route('/predict',methods=['POST'])
+def predict():    
+   '''    
+    For rendering results on HTML GUI    
+   '''    
+   int_features = [int(x) for x in request.form.values()]
+   final_features=[np.array(int_features)]
+   prediction = model.predict(final_features)     
+   output = round(prediction[0], 2)
+   return render_template('page.html', prediction_text='Covid 19 cases would be {}'.format(output))
+if __name__ == "Covid19":     
+   app.run(debug=True)
+
+
+# In[ ]:
+
+
+
+
---
+++ models.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+print("Hello")
+
+
+# In[2]:
+
+
+import pandas as pd
+
+#create dataset
+df = pd.DataFrame({'hours': [1, 2, 4, 5, 5],
+                   'score': [64, 66, 76, 73, 74]})
+df     
+
+
+# In[3]:
+
+
+#define response variable
+Y = df['score']
+
+#define explanatory variable
+X = df[['hours']]
+
+#add constant to predictor variables
+
+regressor = LinearRegression() #Fitting model with training data
+regressor.fit(X, Y)
+
+
+# In[4]:
+
+
+#pickle.dump(regressor, open('model.pkl','wb'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
