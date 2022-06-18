4f25314dc651b7a24a95afcab091d0ebd8c35c93
tinak6501 <89550132+tinak6501@users.noreply.github.com>
2022-06-18 03:07:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 #Step 3
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb')) 
+
 @app.route('/')
 def home():    
-   return render_template('page.html') 
-@app.route('/predict',methods=['POST'])
-def predict():    
-   '''    
-    For rendering results on HTML GUI    
-   '''    
-   int_features = [int(x) for x in request.form.values()]
-   final_features=[np.array(int_features)]
-   prediction = model.predict(final_features)     
-   output = round(prediction[0], 2)
-   return render_template('page.html', prediction_text='Price of the Product should be $ {}'.format(output))
-if __name__ == "__main__":    
-   app.run(debug=True)
+   return "HEllo WORLD"
 
-
-# In[ ]:
+if __name__ == "__main__":     
+   app.run()
 
 
 
