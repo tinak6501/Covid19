5d7c67e8da53105f7e08bd0375e231bbacd12350
tinak6501 <89550132+tinak6501@users.noreply.github.com>
2022-06-28 19:33:59 -0400

    Update app.py

--- Covid19-main/app.py
+++ Covid19-main/app.py
@@ -2,22 +2,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-from sktime.forecasting.compose import ForecastingPipeline
-from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.base import ForecastingHorizon
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-#from sktime.datasets import load_airline
-#from sktime.forecasting.model_selection import temporal_train_test_split
-#from sktime.utils.plotting.forecasting import plot_ys
-
-
-from sktime.forecasting.base import ForecastingHorizon
-#from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.performance_metrics.forecasting import mean_absolute_percentage_error
-from sktime.utils.plotting import plot_series
 
 
 import pandas as pd
@@ -33,25 +21,101 @@ def home():
 
 
 def predict():
-    df = pd.read_csv('vaccines_by_age.csv', parse_dates=['Date'])
-    df = df[['Date', 'Agegroup', 'At least one dose_cumulative']]
-    df = df[df['Date'] > '2021-06-01'].reset_index(drop=True)
-    df1 = df.pivot_table(index=['Date'], columns='Agegroup', values=['At least one dose_cumulative']).reset_index()
-    df1.columns = ['Date', '05-11yrs', '12-17yrs', '18-29yrs', '30-39yrs', '40-49yrs', '50-59yrs', '60-69yrs',
-                   '70-79yrs', '80', 'Adults_18plus', 'Ontario_12plus', 'Ontario_5plus', 'Undisclosed_or_missing']
-    df2 = df1[['Date', '12-17yrs', '18-29yrs', '30-39yrs', '40-49yrs',
-               '50-59yrs', '60-69yrs', '70-79yrs', '80', 'Adults_18plus', 'Ontario_12plus']]
-    df2 = df2.set_index('Date')
+    
+
+    df=pd.read_csv('/Users/kasturid3/Desktop/vaccines_by_age.csv',parse_dates=['Date'])
+
+
+    # In[32]:
+
+
+    df
+
+
+    # In[33]:
+
+
+    df=df[['Date','Agegroup','At least one dose_cumulative']]
+
+
+    # In[34]:
+
+
+    df=df[df['Date']>'2021-06-01'].reset_index(drop=True)
+
+
+    # In[35]:
+
+
+    df1=df.pivot_table(index=['Date'],columns='Agegroup',values=['At least one dose_cumulative']).reset_index()
+
+
+    # In[36]:
+
+
+    df1.columns=['Date','05-11yrs','12-17yrs','18-29yrs','30-39yrs','40-49yrs','50-59yrs','60-69yrs','70-79yrs','80','Adults_18plus','Ontario_12plus','Ontario_5plus','Undisclosed_or_missing']
+
+
+    # In[37]:
+
+
+    df2=df1[['Date','12-17yrs', '18-29yrs', '30-39yrs', '40-49yrs',
+           '50-59yrs', '60-69yrs', '70-79yrs', '80','Adults_18plus', 'Ontario_12plus']]
+
+
+    # In[38]:
+
+
+    df2
+
+
+    # In[39]:
+
+
+    df2=df2.set_index('Date')
+
+
+    # In[40]:
+
 
     df2.index = pd.to_datetime(df2.index)
 
-    df2.index = df2.index.to_period("D")
+
+    # In[41]:
+
+
+    df2.index=df2.index.to_period("D")
+
+
+    # In[42]:
+
 
     y = pd.Series(df2['12-17yrs'])
 
-    y_train, y_test = temporal_train_test_split(y, test_size=24)
-    fh = ForecastingHorizon(y_test.index, is_relative=False)
-    prediction = model.predict(fh=fh)     
+
+    # In[43]:
+
+
+    train=df2['12-17yrs'][:280]
+    test=df2['12-17yrs'][280:]
+
+
+    # In[44]:
+
+
+    test
+
+
+    # In[28]:
+
+
+    from statsmodels.tsa.arima_model import ARIMA
+
+    # 1,1,2 ARIMA Model
+    model = ARIMA(train, order=(1,1,2))
+    model=model.fit()
+    prediction=model.predict(start=1, end=24, exog=None, dynamic=False)
+    
     output = round(prediction)
     print(output)
     pred_text=''
