47dea2359976fa5ace520c552c124d7ae092dab7
tinak6501 <89550132+tinak6501@users.noreply.github.com>
2022-06-28 23:20:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():    
    
-   df=pd.read_csv('/Users/kasturid3/Desktop/vaccines_by_age.csv',parse_dates=['Date'])
+   df=pd.read_csv('vaccines_by_age.csv',parse_dates=['Date'])
 
 
    df=df[['Date','Agegroup','At least one dose_cumulative']]
@@ -30,20 +30,16 @@ def predict():
 
    df1.columns=['Date','05-11yrs','12-17yrs','18-29yrs','30-39yrs','40-49yrs','50-59yrs','60-69yrs','70-79yrs','80','Adults_18plus','Ontario_12plus','Ontario_5plus','Undisclosed_or_missing']
 
-
    df2=df1[['Date','12-17yrs', '18-29yrs', '30-39yrs', '40-49yrs',
           '50-59yrs', '60-69yrs', '70-79yrs', '80','Adults_18plus', 'Ontario_12plus']]
 
-
    df2=df2.set_index('Date')
-
 
    df2.index = pd.to_datetime(df2.index)
 
    df2.index=df2.index.to_period("D")
 
    y = pd.Series(df2['12-17yrs'])
-
 
   
    train=df2['12-17yrs'][:280]
@@ -70,6 +66,7 @@ def predict():
    prediction=forecast[['yhat']]   
    output = round(prediction)
    return render_template('page.html', prediction_text=str(output))
+
 if __name__ == "__main__":    
    app.run(debug=True)
   
