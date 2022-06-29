d6d7e54e23744b781afbfecaf599caccc3563fff
tinak6501 <89550132+tinak6501@users.noreply.github.com>
2022-06-28 20:03:15 -0400

    Add files via upload

---
+++ Covid19-main/Forecasting (3) (1).py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[61]:
+
+
+import pandas as pd
+
+import pandas as pd
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle 
+
+
+from statsmodels.tsa.arima_model import ARIMA
+
+
+# In[46]:
+
+
+df=pd.read_csv('/Users/kasturid3/Desktop/vaccines_by_age.csv',parse_dates=['Date'])
+
+
+# In[47]:
+
+
+df=df[['Date','Agegroup','At least one dose_cumulative']]
+
+
+# In[48]:
+
+
+df=df[df['Date']>'2021-06-01'].reset_index(drop=True)
+
+
+# In[49]:
+
+
+df1=df.pivot_table(index=['Date'],columns='Agegroup',values=['At least one dose_cumulative']).reset_index()
+
+
+# In[50]:
+
+
+df1.columns=['Date','05-11yrs','12-17yrs','18-29yrs','30-39yrs','40-49yrs','50-59yrs','60-69yrs','70-79yrs','80','Adults_18plus','Ontario_12plus','Ontario_5plus','Undisclosed_or_missing']
+
+
+# In[51]:
+
+
+df2=df1[['Date','12-17yrs', '18-29yrs', '30-39yrs', '40-49yrs',
+       '50-59yrs', '60-69yrs', '70-79yrs', '80','Adults_18plus', 'Ontario_12plus']]
+
+
+# In[52]:
+
+
+df2
+
+
+# In[53]:
+
+
+df2=df2.set_index('Date')
+
+
+# In[54]:
+
+
+df2.index = pd.to_datetime(df2.index)
+
+
+# In[55]:
+
+
+df2.index=df2.index.to_period("D")
+
+
+# In[56]:
+
+
+y = pd.Series(df2['12-17yrs'])
+
+
+# In[57]:
+
+
+train=df2['12-17yrs'][:280]
+test=df2['12-17yrs'][280:]
+
+
+# In[58]:
+
+
+test
+
+
+# In[59]:
+
+
+
+
+# 1,1,2 ARIMA Model
+model = ARIMA(train, order=(1,1,2))
+model=model.fit()
+#prediction=model.predict(start=1, end=24, exog=None, dynamic=False)
+
+
+# In[60]:
+
+
+pred
+
+
+# In[22]:
+
+
+rmse = np.mean((y_pred - y_test)**2)**.5  # RMSE
+rmse
+
+
+# In[ ]:
+
+
+
+
